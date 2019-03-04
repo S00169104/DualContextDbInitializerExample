5c0449243b72b23bd31c19313b714f7e8d049aca
ITSPaul <powell.paul@itsligo.ie>
2019-03-04 10:01:59 +0000

    Initial Commit

--- DualContextDbInitializerExample/Global.asax.cs
+++ DualContextDbInitializerExample/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using UIControlsExample.Models;
 
 namespace DualContextDbInitializerExample
 {
@@ -13,11 +14,13 @@ namespace DualContextDbInitializerExample
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            UIContext db = new UIContext();
         }
     }
 }
---
+++ DualContextDbInitializerExample/Models/ApplicationContextInitializer.cs
@@ -0,0 +1,41 @@
+﻿using DualContextDbInitializerExample.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace UIControlsExample.Models
+{
+    public class ApplicationContextInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        public ApplicationContextInitializer()
+        {
+            // Will Fire when App starts up for the first time
+        }
+        protected override void Seed(ApplicationDbContext context)
+        {
+            Random r = new Random();
+            PasswordHasher hasher = new PasswordHasher();
+
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+
+                    UserName = "powell.paul@itsligo.ie",
+                    Email = "powell.paul@itsligo.ie",
+                    EmailConfirmed = true,
+                    FirstName = "Paul",
+                    SecondName = "Powell",
+                    PasswordHash = hasher.HashPassword("itsPaul$1"),
+                    SecurityStamp = Guid.NewGuid().ToString()
+                });
+            context.SaveChanges();
+            base.Seed(context);
+        }
+
+    }
+}
